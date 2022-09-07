ba7be7dbd8b53036a4b4d5172762bddd51e416a0
Jacob Ebey <jacob.ebey@live.com>
2022-09-07 16:25:46 -0700

    log env

--- runtime.ts
+++ runtime.ts
@@ -725,6 +725,7 @@ export function buildChecksum({
   appDirectory: string;
   assetsDirectory?: string;
 }) {
+  console.log(Deno.env.toObject());
   const deploymentId = Deno.env.get("DENO_DEPLOYMENT_ID");
   if (deploymentId) return deploymentId;
 
